008dceb19283952e499712f31799329367144d9f
d-sayali <sayali08d@gmail.com>
2025-03-11 17:12:04 +0530

    Test case to handle \n delimeter

--- tests/test_string_calc.py
+++ tests/test_string_calc.py
@@ -16,3 +16,7 @@ def test_two_numbers():
 def test_multiple_numbers():
     assert StringCalculator.add("1,2,3,4,5") == 15
 
+
+def test_newline_as_delimiter():
+    assert StringCalculator.add("1\n2,3") == 6
+

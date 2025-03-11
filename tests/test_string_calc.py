3a5088bf8181308e97f63803ac368393cf5002f9
d-sayali <sayali08d@gmail.com>
2025-03-11 17:07:28 +0530

    Second case to add single integer value

--- tests/test_string_calc.py
+++ tests/test_string_calc.py
@@ -3,3 +3,8 @@ from src.calculator import StringCalculator
 
 def test_empty_string():
     assert StringCalculator.add("") == 0
+
+
+def test_single_number():
+    assert StringCalculator.add("1") == 1
+
